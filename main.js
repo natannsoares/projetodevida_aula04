aef944eaa0b1d1f0dc6e28fc124bdb0a0bbb9463
natannsoares <119411819+natannsoares@users.noreply.github.com>
2024-03-26 10:32:03 -0300

    aula04

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// Seleciona todos os elementos com a classe ".botao"
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
-// Loop para cada botão
 for (let i = 0; i < botoes.length; i++) {
-    // Quando um botão é clicado, executa esta função
     botoes[i].onclick = function () {
-        // Remove a classe "ativo" de todos os botões
+
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
-        // Adiciona a classe "ativo" apenas ao botão clicado
+
         botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
     }
 }
